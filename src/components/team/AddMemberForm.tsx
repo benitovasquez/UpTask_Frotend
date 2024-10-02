4ced7c239a275211ee51d50e75671568fa609fc7
benitovasquez <ricardobenitovasquezroca@gmail.com>
2024-10-02 09:29:34 -0400

    Corrigiendo problema con team

--- src/components/team/AddMemberForm.tsx
+++ src/components/team/AddMemberForm.tsx
@@ -69,7 +69,7 @@ export default function AddMemberForm() {
             <div className="mt-10">
                 {mutation.isPending && <p className="text-center">Cargando ...</p>}
                 {mutation.error && <p className="text-center">{mutation.error.message}</p>}
-                {mutation.data && <SearchResult user={mutation.data.user} reset={resetData}/>}
+                {mutation.data && <SearchResult user={mutation.data} reset={resetData}/>}
             </div>
         </>
     )
